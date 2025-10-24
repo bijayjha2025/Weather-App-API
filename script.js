2456ca4c7ca0448a7af73bd7b90dcf32b665b435
Bijay Jha <bijayjha12345@gmail.com>
2025-10-24 10:54:04 +0545

    Adding constants and linking respective classes using querySelector

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 
-
 const searchInput = document.querySelector(".searchCity input");
 const searchButton = document.querySelector(".searchCity button");
 
+const weatherIcon = document.querySelector(".weatherIcon");
+const temperatureShow = document.querySelector(".temperatureShow");
+const cityName = document.querySelector(".cityName");
+const humidityValue = document.querySelector(".humidity");
+const windValue = document.querySelector(".wind");
+
+const apiKey = "api-key";
 
